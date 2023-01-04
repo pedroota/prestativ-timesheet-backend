9a1763f3398787a87752b10c43906621fd182bba
Arthur Luan <arthurluancouto214@gmail.com>
2023-01-04 15:20:04 +0000

    fix: error handler unused var type Next

--- src/app/middlewares/errorHandler.ts
+++ src/app/middlewares/errorHandler.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import { Request, Response, ErrorRequestHandler } from "express";
 
 export function errorHandler(
   error: ErrorRequestHandler,
