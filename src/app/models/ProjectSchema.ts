9b98003d7c912c4267aebb2595ee9e9f885b40a3
Arthur Luan <arthurluancouto214@gmail.com>
2023-01-04 22:01:54 +0000

    feat: project schema

---
+++ src/app/models/ProjectSchema.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+interface IProject {
+  title: string;
+  idClient: mongoose.Schema.Types.ObjectId;
+  valueProject: number;
+  gpProject: string;
+  description: string;
+  createdAt: number;
+  updatedAt: number;
+}
+
+const ProjectSchema = new Schema<IProject>({
+  title: {
+    type: String,
+    required: true,
+  },
+  idClient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+    required: true,
+  },
+  valueProject: {
+    type: Number,
+    required: true,
+  },
+  gpProject: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Number,
+    required: true,
+  },
+  updatedAt: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Project = mongoose.model<IProject>("Project", ProjectSchema);
+
+module.exports = Project;
