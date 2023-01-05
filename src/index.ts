ea2fd155abbbb67bfa4fb84db104b672ce3b4ad6
Arthur Luan <arthurluancouto214@gmail.com>
2023-01-05 14:37:46 +0000

    feat: config cors

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ dotenv.config();
 const cookieParser = require("cookie-parser");
 // import { errorHandler } from "./app/middlewares/errorHandler";
 import "./database/db";
+const cors = require("cors");
 const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 // app.use(errorHandler);
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.SERVER_PORT, () => {
