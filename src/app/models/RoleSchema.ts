25d72b7e36d714f48b5a863aac973b6d04de5f7a
Arthur Luan <arthurluancouto214@gmail.com>
2023-01-05 13:49:21 +0000

    feat: add validation for role registration

--- src/app/models/RoleSchema.ts
+++ src/app/models/RoleSchema.ts
@@ -9,6 +9,8 @@ interface IRole {
 const RoleSchema = new Schema<IRole>({
   name: {
     type: String,
+    enum: ["Administrador", "Operação", "Gestor de Projetos", "Consultor"],
+    default: "Consultor",
     required: true,
   },
 });
