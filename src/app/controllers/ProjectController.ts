f2c13c76cd7c9821df23a680013959ca453b684c
Filipe Bacof01~ <filipebacof@gmail.com>
2023-01-06 18:20:31 -0300

    atualizados bugs nos CRUDs update + CRUD PROJETO

---
+++ src/app/controllers/ProjectController.ts
@@ -0,0 +1,76 @@
+import { Request, Response } from "express";
+const ProjectRepository = require("../repositories/ProjectRepository");
+
+class ProjectController {
+  async index(request: Request, response: Response) {
+    const projects = await ProjectRepository.findAll();
+
+    return response.json(projects);
+  }
+
+  // show(request: Request, response: Response) {}
+
+  async store(request: Request, response: Response) {
+    const { title, idClient, valueProject, gpProject, description } =
+      request.body;
+
+    const isProjectAlreadyRegistered = await ProjectRepository.findByName(
+      title
+    );
+
+    if (isProjectAlreadyRegistered)
+      return response
+        .status(422)
+        .json({ message: "Um projeto com este nome já foi cadastrado" });
+
+    const project = await ProjectRepository.create({
+      title,
+      idClient,
+      valueProject,
+      gpProject,
+      description,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
+
+    return response.status(200).json(project);
+  }
+
+  async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { title, idClient, valueProject, gpProject, description } =
+      request.body;
+
+    const updatedProject = await ProjectRepository.findByIdAndUpdate({
+      id,
+      title,
+      idClient,
+      valueProject,
+      gpProject,
+      description,
+    });
+
+    return response
+      .status(200)
+      .json({ message: "Projeto atualizado com sucesso.", updatedProject });
+  }
+
+  async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const project = await ProjectRepository.findById(id);
+
+    if (!project)
+      return response
+        .status(404)
+        .json({ message: "Este projeto não foi encontrado." });
+
+    await ProjectRepository.delete(id);
+
+    return response
+      .status(204)
+      .json({ message: "Projeto deletado com sucesso." });
+  }
+}
+
+module.exports = new ProjectController();
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 const UsersController = require("./app/controllers/UserController");
 const RoleController = require("./app/controllers/RoleController");
 const ClientController = require("./app/controllers/ClientController");
+const ProjectController = require("./app/controllers/ProjectController");
 import { verifyTokenJwt } from "./app/middlewares/verifyTokenJwt";
 const router = Router();
 
@@ -13,13 +14,19 @@ router.get("/auth/users", verifyTokenJwt, UsersController.index);
 // Role Routes
 router.get("/roles", verifyTokenJwt, RoleController.index);
 router.post("/roles", verifyTokenJwt, RoleController.store);
-router.patch("/roles/:id", verifyTokenJwt, RoleController.update);
+router.put("/roles/:id", verifyTokenJwt, RoleController.update);
 router.delete("/roles/:id", verifyTokenJwt, RoleController.delete);
 
 // Client Routes
 router.get("/clients", verifyTokenJwt, ClientController.index);
 router.post("/clients", verifyTokenJwt, ClientController.store);
-router.patch("/clients/:id", verifyTokenJwt, ClientController.update);
+router.put("/clients/:id", verifyTokenJwt, ClientController.update);
 router.delete("/clients/:id", verifyTokenJwt, ClientController.delete);
+
+// Project Routes
+router.get("/projects", ProjectController.index);
+router.post("/projects", ProjectController.store);
+router.put("/projects/:id", ProjectController.update);
+router.delete("/projects/:id", ProjectController.delete);
 
 module.exports = router;
