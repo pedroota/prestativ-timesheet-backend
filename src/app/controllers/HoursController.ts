f5f96bd38d59cb9fc8e08794992b42473a10f29a
Filipe Bacof01~ <filipebacof@gmail.com>
2023-02-16 16:08:17 -0300

    get all with pagination and/or specific date

--- src/app/controllers/HoursController.ts
+++ src/app/controllers/HoursController.ts
@@ -6,6 +6,32 @@ const ProjectRepository = require("../repositories/ProjectRepository");
 class HoursController {
   async index(request: Request, response: Response) {
     const page = Number(request.query.page);
+    const date = Number(request.query.date);
+    // "date" é um timestamp, basta gerar um timestamp de qualquer dia do mês desejado
+
+    if (date && page) {
+      const startIndex = (page - 1) * 10;
+
+      const hours = await HoursRepository.findSpecificMonthPaginated(
+        date,
+        startIndex
+      );
+
+      hours.sort(function (x: { initial: number }, y: { initial: number }) {
+        return x.initial - y.initial;
+      });
+
+      return response.json(hours);
+    }
+    if (date) {
+      const hours = await HoursRepository.findSpecificMonthPaginated(date);
+
+      hours.sort(function (x: { initial: number }, y: { initial: number }) {
+        return x.initial - y.initial;
+      });
+
+      return response.json(hours);
+    }
 
     if (page) {
       const startIndex = (page - 1) * 10;
