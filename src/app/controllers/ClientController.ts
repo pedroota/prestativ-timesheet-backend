82d76ad25cb4ddf3f10f93b6b3a3e37b08927af5
Filipe Bacof <72168940+Filipe-Bacof@users.noreply.github.com>
2023-03-24 21:27:38 -0300

    Add files via upload

--- src/app/controllers/ClientController.ts
+++ src/app/controllers/ClientController.ts
@@ -31,6 +31,82 @@ class ClientController {
       .json({ message: "Cliente encontrado com sucesso", client });
   }
 
+  // o método abaixo não está funcionando como esperado, precisa ajustar a ultima função para filtrar as atividades apenas desse usuário e depois ver quais estão válidas
+  async getInfo(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const clients = await ClientRepository.findAll();
+
+    // Percorra o array de clientes
+    for (let i = 0; i < clients.length; i++) {
+      const cliente = clients[i];
+
+      // Percorra o array de projetos do cliente
+      for (let j = 0; j < cliente.projects.length; j++) {
+        const projeto = cliente.projects[j];
+
+        // Percorra o array de atividades do projeto
+        for (let k = 0; k < projeto.activities.length; k++) {
+          const atividade = projeto.activities[k];
+
+          // Remova os campos desnecessários da atividade
+          delete atividade.project;
+          delete atividade.valueActivity;
+          delete atividade.gpActivity;
+          delete atividade.description;
+          delete atividade.closedScope;
+          delete atividade.businessUnit;
+          delete atividade.createdAt;
+          delete atividade.updatedAt;
+          delete atividade.__v;
+        }
+
+        // Remova os campos desnecessários do projeto
+        delete projeto.idClient;
+        delete projeto.valueProject;
+        delete projeto.gpProject;
+        delete projeto.description;
+        delete projeto.createdAt;
+        delete projeto.updatedAt;
+        delete projeto.businessUnit;
+        delete projeto.__v;
+      }
+
+      // Remova os campos desnecessários do cliente
+      delete cliente.corporateName;
+      delete cliente.cnpj;
+      delete cliente.cep;
+      delete cliente.street;
+      delete cliente.streetNumber;
+      delete cliente.complement;
+      delete cliente.district;
+      delete cliente.city;
+      delete cliente.state;
+      delete cliente.createdAt;
+      delete cliente.updatedAt;
+      delete cliente.periodIn;
+      delete cliente.periodUntil;
+      delete cliente.billingLimit;
+      delete cliente.payDay;
+      delete cliente.valueClient;
+      delete cliente.gpClient;
+      delete cliente.businessUnit;
+      delete cliente.__v;
+    }
+
+    const filteredClients = clients.filter((client) => {
+      const filteredProjects = client.projects.filter((project) => {
+        const filteredActivities = project.activities.filter((activity) =>
+          activity.users.includes(id)
+        );
+        return filteredActivities.length > 0;
+      });
+      return filteredProjects.length > 0;
+    });
+
+    return response.json(filteredClients);
+  }
+
   async store(request: Request, response: Response) {
     const {
       corporateName,
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { verifyTokenForgotPass } from "./app/middlewares/verifyTokenForgotPass";
+import { verifyTokenJwt } from "./app/middlewares/verifyTokenJwt";
 const UsersController = require("./app/controllers/UserController");
 const RoleController = require("./app/controllers/RoleController");
 const ClientController = require("./app/controllers/ClientController");
@@ -11,70 +12,83 @@ const LogController = require("./app/controllers/LogController");
 const router = Router();
 
 // User Routes
-router.post("/auth/register", UsersController.register);
+router.post("/auth/register", verifyTokenJwt, UsersController.register);
 router.post("/auth/login", UsersController.login);
-router.get("/auth/users", UsersController.index);
-router.get("/auth/users/:id", UsersController.show);
-router.put("/auth/users/:id", UsersController.update);
-router.delete("/auth/users/:id", UsersController.delete);
+router.get("/auth/users", verifyTokenJwt, UsersController.index);
+router.get("/auth/users/:id", verifyTokenJwt, UsersController.show);
+router.put("/auth/users/:id", verifyTokenJwt, UsersController.update);
+router.delete("/auth/users/:id", verifyTokenJwt, UsersController.delete);
 router.post("/auth/forgot", UsersController.forgot);
 router.post("/auth/newpass", verifyTokenForgotPass, UsersController.newPass);
 
 // Role Routes
-router.get("/roles", RoleController.index);
-router.get("/roles/:id", RoleController.show);
-router.post("/roles", RoleController.store);
-router.put("/roles/:id", RoleController.update);
-router.delete("/roles/:id", RoleController.delete);
+router.get("/roles", verifyTokenJwt, RoleController.index);
+router.get("/roles/:id", verifyTokenJwt, RoleController.show);
+router.post("/roles", verifyTokenJwt, RoleController.store);
+router.put("/roles/:id", verifyTokenJwt, RoleController.update);
+router.delete("/roles/:id", verifyTokenJwt, RoleController.delete);
 
 // Client Routes
-router.get("/clients", ClientController.index);
-router.get("/clients/:id", ClientController.show);
-router.post("/clients", ClientController.store);
-router.put("/clients/:id", ClientController.update);
-router.delete("/clients/:id", ClientController.delete);
+router.get("/clients", verifyTokenJwt, ClientController.index);
+router.get("/clients/:id", verifyTokenJwt, ClientController.show);
+router.post("/clients", verifyTokenJwt, ClientController.store);
+router.put("/clients/:id", verifyTokenJwt, ClientController.update);
+router.delete("/clients/:id", verifyTokenJwt, ClientController.delete);
+router.get("/clients/user/:id", verifyTokenJwt, ClientController.getInfo);
 
 // Project Routes
-router.get("/projects", ProjectController.index);
-router.get("/projects/:id", ProjectController.show);
-router.post("/projects", ProjectController.store);
-router.put("/projects/:id", ProjectController.update);
-router.delete("/projects/:id", ProjectController.delete);
+router.get("/projects", verifyTokenJwt, ProjectController.index);
+router.get("/projects/:id", verifyTokenJwt, ProjectController.show);
+router.post("/projects", verifyTokenJwt, ProjectController.store);
+router.put("/projects/:id", verifyTokenJwt, ProjectController.update);
+router.delete("/projects/:id", verifyTokenJwt, ProjectController.delete);
 
 // Activity Routes
-router.get("/activities", ActivityController.index);
-router.get("/activities/:id", ActivityController.show);
-router.get("/active/activities", ActivityController.active);
-router.patch("/active/activities/:id", ActivityController.validity);
-router.post("/activities", ActivityController.store);
-router.put("/activities/:id", ActivityController.update);
-router.delete("/activities/:id", ActivityController.delete);
-router.patch("/activities/closedscope/:id", ActivityController.check);
+router.get("/activities", verifyTokenJwt, ActivityController.index);
+router.get("/activities/:id", verifyTokenJwt, ActivityController.show);
+router.get("/active/activities", verifyTokenJwt, ActivityController.active);
+router.patch(
+  "/active/activities/:id",
+  verifyTokenJwt,
+  ActivityController.validity
+);
+router.post("/activities", verifyTokenJwt, ActivityController.store);
+router.put("/activities/:id", verifyTokenJwt, ActivityController.update);
+router.delete("/activities/:id", verifyTokenJwt, ActivityController.delete);
+router.patch(
+  "/activities/closedscope/:id",
+  verifyTokenJwt,
+  ActivityController.check
+);
 
 // Hours Routes
-router.get("/hours", HoursController.index);
-router.get("/hours/user/:id", HoursController.indexByUser);
-router.get("/hours/filter", HoursController.filter);
-router.get("/hours/latest", HoursController.latest);
-router.get("/hours/:id", HoursController.show);
-router.post("/hours", HoursController.store);
-router.put("/hours/:id", HoursController.update);
-router.patch("/hours/releasedcall/:id", HoursController.updateReleasedCall);
-router.delete("/hours/:id", HoursController.delete);
-router.patch("/hours/check/:id", HoursController.check);
+router.get("/hours", verifyTokenJwt, HoursController.index);
+router.get("/hours/user/:id", verifyTokenJwt, HoursController.indexByUser);
+router.get("/hours/filter", verifyTokenJwt, HoursController.filter);
+router.get("/hours/latest", verifyTokenJwt, HoursController.latest);
+router.get("/hours/:id", verifyTokenJwt, HoursController.show);
+router.post("/hours", verifyTokenJwt, HoursController.store);
+router.put("/hours/:id", verifyTokenJwt, HoursController.update);
+router.patch(
+  "/hours/releasedcall/:id",
+  verifyTokenJwt,
+  HoursController.updateReleasedCall
+);
+router.delete("/hours/:id", verifyTokenJwt, HoursController.delete);
+router.patch("/hours/check/:id", verifyTokenJwt, HoursController.check);
 
 // Logs
-router.get("/logs", LogController.index);
-router.get("/logs/:id", LogController.show);
-router.post("/logs", LogController.store);
-router.put("/logs/:id", LogController.update);
-router.delete("/logs/:id", LogController.delete);
+router.get("/logs", verifyTokenJwt, LogController.index);
+router.get("/logs/:id", verifyTokenJwt, LogController.show);
+router.post("/logs", verifyTokenJwt, LogController.store);
+router.put("/logs/:id", verifyTokenJwt, LogController.update);
+router.delete("/logs/:id", verifyTokenJwt, LogController.delete);
 
 // Business Unit Routes
-router.get("/business", BusinessController.index);
-router.get("/business/:id", BusinessController.show);
-router.post("/business", BusinessController.store);
-router.put("/business/:id", BusinessController.update);
-router.delete("/business/:id", BusinessController.delete);
+router.get("/business", verifyTokenJwt, BusinessController.index);
+router.get("/business/:id", verifyTokenJwt, BusinessController.show);
+router.post("/business", verifyTokenJwt, BusinessController.store);
+router.put("/business/:id", verifyTokenJwt, BusinessController.update);
+router.delete("/business/:id", verifyTokenJwt, BusinessController.delete);
 
 module.exports = router;
